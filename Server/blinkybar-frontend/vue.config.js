034509b59bc211c9b2f386f91893b63422fb59d4
Stefan Lobas <stefanlobas@googlemail.com>
2021-07-05 19:51:11 +0200

    Added config file for local testing. Start cherrypy server first so that the dev server grabs another port than 8080.

--- Server/blinkybar-frontend/vue.config.js
+++ Server/blinkybar-frontend/vue.config.js
@@ -1,5 +1,12 @@
 const path = require("path");
 
 module.exports = {
-    outputDir: path.resolve(__dirname, "../static")
+    outputDir: path.resolve(__dirname, "../static"),
+    devServer: {
+        proxy: {
+            '/.*': {
+                target: 'http://localhost:8080'
+            },
+        }
+    }
 }
