dfdad44e108f9f6e27b1c61df1dee14c3d104a4d
Fabian Schwartau <fabian@opencode.eu>
2021-07-09 19:10:59 +0200

    Moved color temperature table to an extra file

--- Server/blinkybar.py
+++ Server/blinkybar.py
@@ -13,6 +13,7 @@ import hashlib
 import configparser
 import numpy as np
 from message import *
+from colortemperaturetable import *
 
 
 # Format cherrypy logging with ms output
@@ -114,30 +115,6 @@ class ModuleController(Thread):
             "progress_msg": ""
         }
 
-        # Intialize color temperature table
-
-        self.color_temperature_table = {
-            1000: (255,56,0),
-            1500: (255,109,0),
-            2000: (255,137,18),
-            2500: (255,161,72),
-            3000: (255,180,107),
-            3500: (255,196,137),
-            4000: (255,209,163),
-            4500: (255,219,186),
-            5000: (255,228,206),
-            5500: (255,236,224),
-            6000: (255,243,239),
-            6500: (255,249,253),
-            7000: (245,243,255),
-            7500: (235,238,255),
-            8000: (227,233,255),
-            8500: (220,229,255),
-            9000: (214,225,255),
-            9500: (208,222,255),
-            10000: (204,219,255)
-        }
-
         # Initialize the modules
         self.init_modules()
 
@@ -166,11 +143,12 @@ class ModuleController(Thread):
                 # Apply color temperature correction
                 temp_sel = self.led_settings['color_temperature']
                 # Get the closest RGB values from the table
-                r, g, b = self.color_temperature_table.get(temp_sel) or self.color_temperature_table[min(self.color_temperature_table.keys(), key = lambda key: abs(key-temp_sel))]
+                r, g, b = get_color_temperatures(temp_sel)
                 matrix = (r/255.0, 0.0, 0.0, 0.0,
                     0.0, g/255.0, 0.0, 0.0,
                     0.0, 0.0, b/255.0, 0.0)
                 image_scaled = self.image.convert('RGB', matrix)
+                cherrypy.log(f"Applied color temperature of {temp_sel} K")
 
                 # Apply brightness correction
                 enhancer = ImageEnhance.Brightness(image_scaled)
