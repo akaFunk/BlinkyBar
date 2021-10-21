918d76b9d15a06d1325a39491f96b2937bbabc18
Fabian Schwartau <fabian@opencode.eu>
2021-10-21 18:45:45 +0200

    optimized fifo

--- Module_Code/fifo.c
+++ Module_Code/fifo.c
@@ -8,6 +8,7 @@ void fifo_init(fifo_t* fifo, uint16_t size, uint8_t* data)
 	fifo->wptr = data;
 	fifo->used = 0;
 	fifo->size = size;
+	fifo->end = data + size;
 }
 
 void fifo_pushc(fifo_t* fifo, uint8_t data)
@@ -21,32 +22,30 @@ void fifo_pushc(fifo_t* fifo, uint8_t data)
 	fifo->used++;
 	*fifo->wptr = data;
 	fifo->wptr++;
-	if(fifo->wptr == fifo->data+fifo->size)
+	if(fifo->wptr == fifo->end)
 		fifo->wptr = fifo->data;
 	sei();
 }
 
 void fifo_pushcc(fifo_t* fifo, uint8_t* data, uint16_t length)
 {
-    for(uint16_t i=0; i<length; i++)
-    {
+    for(uint16_t i = 0; i < length; i++)
         fifo_pushc(fifo, data[i]);
-    }
 }
 
 uint8_t fifo_popc(fifo_t* fifo)
 {
+	uint8_t data;
 	cli();
-	uint8_t data=0;
-	data = *fifo->rptr;
 	if(!fifo->used)
 	{
 		sei();
 		return 0;
 	}
+	data = *fifo->rptr;
 	fifo->used--;
 	fifo->rptr++;
-	if(fifo->rptr == fifo->data+fifo->size)
+	if(fifo->rptr == fifo->end)
 		fifo->rptr = fifo->data;
 	sei();
 	return data;
@@ -60,10 +59,8 @@ uint8_t fifo_popc_block(fifo_t* fifo)
 
 void fifo_popcc(fifo_t* fifo, uint8_t* data, uint16_t length)
 {
-    for(uint16_t i=0; i<length; i++)
-    {
+    for(uint16_t i = 0; i < length; i++)
         data[i] = fifo_popc(fifo);
-    }
 }
 
 // Read a character at a given position pos without removing it from
@@ -72,7 +69,7 @@ extern uint8_t fifo_readc(fifo_t* fifo, uint16_t pos)
 {
 	cli();
 	uint8_t* ptr = fifo->rptr+pos;
-	if(ptr >= fifo->data + fifo->size)
+	if(ptr >= fifo->end)
 		ptr -= fifo->size;
 	if(!fifo->used)
 	{
@@ -85,8 +82,8 @@ extern uint8_t fifo_readc(fifo_t* fifo, uint16_t pos)
 
 uint16_t fifo_used(fifo_t* fifo)
 {
+	uint16_t used;
 	cli();
-	uint16_t used=0;
 	used = fifo->used;
 	sei();
 	return used;
