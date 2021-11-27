f2d0d8106b5b261a2946c1584bcd148dc47e8cf8
Fabian Schwartau <fabian@opencode.eu>
2021-11-27 19:35:31 +0100

    Initial version of the main AVR's code - should work, but is not fully tested yet

---
+++ main_code/README.md
@@ -0,0 +1,31 @@
+# Main PCB AVR code
+This folder contains the code for the AVR on the main PCB. The purpose of this controller is battery monitoring, power management, and generation of the trigger signal for the modules.
+
+Once the controller turns on by pushing the button connected to the `MASTER_ON` signal, it is responsible to set `PWR_EN` high to keep the power supply turned on. If the battery voltage is too low, the AVR will turn off the power supply by driving the `PWR_EN` line low. This will also turn off the power of the AVR itself. Before that it will notify the Pi, so the Pi can shut down the modules as they will not be needed if the main module has no power.
+
+The AVR talks to the Raspberry Pi using an SPI. On the Pi side the SPI1 is used in master mode, which needs to be activated using an additional overlay. See the readme file in `buildroot_external` folder for details on that. The AVR uses its only internal hardware SPI, which is shared with the `ISP` interface for promming the AVR. The two 330 Ohm resistors `R7` and `R8` make sure that there are no collisions while debugging and the `ISP` interface is the primary one. The AVR runs at 8 MHz. 16 MHz is not possible, as the supply voltage is only 3.3V.
+
+The Pi will set the period and on time of the trigger signal, how many colums there are and it will start/stop the trigger signal.
+
+## SPI protocoll
+The SPI clock should be fairly low, to make sure that the AVR can keep up. Don't know the exact value, 10 kHz works.
+
+The protocoll is a single packet of 5 bytes in both directions. These 5 bytes have to be encapsulated in a single chip-select sequence. The packet from the Pi to the AVR contains the following data, all values are little endian:
+
+| Bytes | Meaning |
+|---|---|
+| 1 | Command flags: <br> bit 0: start trigger<br>bit 1: stop trigger <br> bit 2: infinite repeat <br> If nothing is set, the Pi only wants to read the voltage/timer_counter and no action is carried out and the local state is not altered. |
+| 2 | Period of the trigger signal, given in µs |
+| 2 | On-time of the trigger signal, given in µs |
+
+And the answer, which is returned in each request contains the following information:
+
+| Bytes | Meaning |
+|---|---|
+| 1 | Dummy, always 0 |
+| 2 | Battery voltage, given in mV |
+| 2 | Current timer counter value of the current progress, which counts from 0 to the period value minus 1 |
+
+TODO: The power button is missing in the packet from AVR to Pi. The Pi needs to know if he has to shut down to shut down all modules first.
+
+TODO: Add a automatic shutdown if we have not heard from the Pi in a while. This would also be helpfull for the modules.
---
+++ main_code/main.c
@@ -0,0 +1,231 @@
+/*
+All values as little endian
+Data Pi -> µC:
+    1 byte: command flags:
+        bit 0: start trigger
+        bit 1: stop trigger
+        bit 2: infinite repeat
+        If nothing is set, the Pi only wants to read the voltage/timer_counter
+        and no action is carried out and the local state is not altered.
+    2 byte: period, given in µs
+    2 byte: on time, given in µs (must be smaller than period)
+Data µC -> Pi:
+    1 byte: dummy, always 0
+    2 byte: Voltage, given in mV
+    2 byte: timer_counter, current progress
+
+TODO: The power button is missing. The Pi needs to know if he has to shut down to shut down all modules.
+TODO: Add a automatic shutdown if we have not heard from the Pi in a while
+ */
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+#include <util/atomic.h>
+
+#define MIN_VOLTAGE      6000   // minimum voltage (mV), below this value the module will turn itself off
+
+#define LED_DDR          DDRC
+#define LED_PORT         PORTC
+#define LED_PIN          2
+#define led_init()       LED_DDR |= (1<<LED_PIN)
+#define led_on()         LED_PORT |= (1<<LED_PIN)
+#define led_off()        LED_PORT &= ~(1<<LED_PIN)
+#define led_toggle()     LED_PORT ^= (1<<LED_PIN)
+
+#define PI_SHTDN_DDR     DDRC
+#define PI_SHTDN_PORT    PORTC
+#define PI_SHTDN_PIN     0
+#define pi_shtdn_init()  PI_SHTDN_DDR |= (1<<PI_SHTDN_PIN)
+#define pi_shtdn_low()   PI_SHTDN_PORT &= ~(1<<PI_SHTDN_PIN)
+#define pi_shtdn_high()  PI_SHTDN_PORT |= (1<<PI_SHTDN_PIN)
+
+#define MASTER_ON_DDR    DDRC
+#define MASTER_ON_PORT   PORTC
+#define MASTER_ON_VALUE  PINC
+#define MASTER_ON_PIN    1
+#define master_on_init() MASTER_ON_DDR &= ~(1<<MASTER_ON_PIN)
+#define master_on_pu()   MASTER_ON_PORT |= (1<<MASTER_ON_PIN)
+#define master_on_val()  (MASTER_ON_VALUE & (1<<MASTER_ON_PIN))
+
+#define PWR_ON_DDR     DDRC
+#define PWR_ON_PORT    PORTC
+#define PWR_ON_PIN     3
+#define pwr_on_init()  PWR_ON_DDR |= (1<<PWR_ON_PIN)
+#define pwr_on_low()   PWR_ON_PORT &= ~(1<<PWR_ON_PIN)
+#define pwr_on_high()  PWR_ON_PORT |= (1<<PWR_ON_PIN)
+
+static uint8_t spi_byte_cnt = 0;
+
+typedef struct __attribute__((__packed__)) {
+    uint8_t command;
+    uint16_t period;
+    uint16_t on_time;
+} command_t;
+static volatile command_t command_buffer;
+static uint8_t *command_buffer_data = (uint8_t*)&command_buffer;
+#define COMMAND_START_TRIGGER   0x01
+#define COMMAND_STOP_TRIGGER    0x02
+#define COMMAND_INFINITE_REPEAT 0x04
+
+typedef struct __attribute__((__packed__)) {
+    uint8_t dummy;
+    uint16_t voltage;
+    uint16_t timer_counter;
+} state_t;
+static volatile state_t state;
+static uint8_t *state_data = (uint8_t*)&state;
+
+// Timer
+static uint16_t timer_period;
+static uint16_t timer_on_time;
+static uint16_t timer_trigger_count;
+static uint8_t timer_infinite_repeat;
+#define timer_on() TCCR1B |= (1<<CS11)
+#define timer_off() TCCR1B &= ~(1<<CS11)
+void timer_init()
+{
+    DDRB |= (1<<1); // Set OC1A as output
+    TIMSK1 = (1<<TOIE1); // Enable overflow interrupt
+    TCCR1A = (1<<COM1A1) | (1<<WGM11);
+    TCCR1B = (1<<WGM12) | (1<<WGM13);  // Fast PWM mode, TOP = ICR1, no clock
+    ICR1 = timer_period - 1;
+    OCR1A = timer_on_time - 1;
+    state.timer_counter = timer_trigger_count;
+}
+
+uint16_t get_voltage()
+{
+    ADCSRA |= (1<<ADSC);  // Start conversion
+    while((ADCSRA & (1<<ADIF)));  // Wait for completion
+    uint16_t adc_val = ADC;
+    ADCSRA |= (1<<ADIF); // Clear interrupt flag
+
+    // convert ADC value to voltage, reference is 1.1V, voltage divider 10/78
+    uint16_t voltage = (uint16_t)(adc_val*8.37);
+    return voltage;
+}
+
+int main()
+{
+    led_init();
+    pi_shtdn_init();
+    pi_shtdn_low();
+    master_on_init();
+    master_on_pu();
+    pwr_on_init();
+    pwr_on_low();
+
+      // Wait 2 seconds before we actually turn on the power while flashing the LED
+    for(int i = 0; i < 20; i++)
+    {
+        _delay_ms(100);
+        led_toggle();
+    }
+    led_on();
+    pwr_on_high();
+
+    // Set up SPI ~SS pin change interrupt
+    PORTB |= (1<<2); // Enable pull-up
+    PCICR |= (1<<PCIE0);
+    PCMSK0 |= (1<<2);
+
+    // Set up SPI slave
+    DDRB |= (1<<4); // Set MISO as output
+    SPCR = (1<<SPIE) | (1<<SPE);
+
+    // Set up battery monitoring ADC
+    ADMUX = (1<<REFS0) | (1<<REFS1) | 7; // 1.1V internal reference, ADC7 source
+    ADCSRA = (1<<ADEN) | (1<<ADPS2) | (1<<ADPS1) | (1<<ADPS0);  // Enable, 1/128 prescaler
+    get_voltage(); // Dummy read
+    _delay_ms(10); // Wait a moment for the bandgap voltage to stabilize
+
+    // Set up trigger timer at OC0A output
+    timer_period = 1000;
+    timer_on_time = 500;
+    timer_trigger_count = 10;
+    timer_infinite_repeat = 0;
+    timer_init();
+    sei();
+
+    while(1)
+    {
+        // Atomically update the battery voltage and only if we are not selected at the moment
+        // This will prevent an update of the voltage while we are currently transmitting it
+        uint16_t voltage = get_voltage();
+        if(PINB & (1<<2))
+        {
+            state.voltage = voltage;
+        }
+        if(voltage < MIN_VOLTAGE)
+        {
+            // Request a shutdown of the Pi
+            pi_shtdn_high();
+            _delay_ms(5000);  // Wait 5 seconds for the Pi to shutdown
+            pwr_on_low();
+        }
+        _delay_ms(1);
+    }
+    return 0;
+}
+
+// SPI transfer complete interrupt
+ISR(SPI_STC_vect)
+{
+    // Save data to buffer
+    command_buffer_data[spi_byte_cnt] = SPDR;
+
+    // Write next byte and increment byte counter
+    // spi_byte_cnt is still 0 after the first byte has been received, so we transmit
+    // state_data[spi_byte_cnt+1]
+    if(spi_byte_cnt + 1 < sizeof(command_t))
+    {
+        SPDR = state_data[spi_byte_cnt+1];
+        spi_byte_cnt++;
+    }
+}
+
+// PB2 aka ~SS pin toggle interrupt
+ISR(PCINT0_vect)
+{
+    // Only trigger on rising edge, i.e., de-selection
+    if(!(PINB & (1<<2)))
+        return;
+    
+    // Set next answer byte to 0 and reset byte cnt
+    SPDR = 0x00;
+    spi_byte_cnt = 0;
+
+    // Interprete the SPI RX buffer
+    if(command_buffer.command)
+    {
+        timer_period = command_buffer.period;
+        timer_on_time = command_buffer.on_time;
+    }
+    timer_infinite_repeat = !!(command_buffer.command & COMMAND_INFINITE_REPEAT);
+    if(command_buffer.command & COMMAND_START_TRIGGER)
+    {
+        timer_init();
+        timer_on();
+    }
+    if(command_buffer.command & COMMAND_STOP_TRIGGER)
+    {
+        timer_off();
+    }
+}
+
+// Timer 1 overflow interrupt
+ISR(TIMER1_OVF_vect)
+{
+    led_toggle();
+    state.timer_counter--; // Count the columns
+    if(!state.timer_counter)
+    {
+        // Reset counter or turn off trigger signal
+        if(timer_infinite_repeat) {
+            state.timer_counter = timer_trigger_count;
+        } else {
+            timer_off();
+        }
+    }
+}
